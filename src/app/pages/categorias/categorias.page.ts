5400095d989340abce7e707faa61b0bc792bbebe
William Willers <w.willers@outlook.com>
2020-06-08 16:42:04 -0400

    Mostrando as categorias

--- src/app/pages/categorias/categorias.page.ts
+++ src/app/pages/categorias/categorias.page.ts
@@ -1,3 +1,5 @@
+import { API_CONFIG } from './../../config/api.config';
+import { CategoriaDTO } from './../../models/categoria.dto';
 import { CategoriaService } from './../../services/domain/categoria.service';
 import { Component, OnInit } from '@angular/core';
 
@@ -8,12 +10,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoriasPage implements OnInit {
 
+  bucketUrl: string = API_CONFIG.bucketBaseUrl
+
+  items: CategoriaDTO[];
+
   constructor(private categoriaService: CategoriaService) { }
 
   ngOnInit() {
     this.categoriaService.findAll()
       .subscribe(response => {
-        console.log(response);
+        this.items = response;
       },
       error => {
         console.log(error);
