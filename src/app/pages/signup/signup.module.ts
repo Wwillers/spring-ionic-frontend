3b2aad6fee34d0e2175d1dd53382611814ceaaa3
William <w.willers@outlook.com>
2020-07-17 16:32:16 -0400

    Povoando dinamicamente Estado e Cidade

--- src/app/pages/signup/signup.module.ts
+++ src/app/pages/signup/signup.module.ts
@@ -7,6 +7,8 @@ import { IonicModule } from '@ionic/angular';
 import { SignupPageRoutingModule } from './signup-routing.module';
 
 import { SignupPage } from './signup.page';
+import { CidadeService } from 'src/app/services/domain/cidade.service';
+import { EstadoService } from 'src/app/services/domain/estado.service';
 
 @NgModule({
   imports: [
@@ -16,6 +18,10 @@ import { SignupPage } from './signup.page';
     IonicModule,
     SignupPageRoutingModule
   ],
-  declarations: [SignupPage]
+  declarations: [SignupPage],
+  providers: [
+    CidadeService,
+    EstadoService
+  ]
 })
 export class SignupPageModule {}
--- src/app/pages/signup/signup.page.ts
+++ src/app/pages/signup/signup.page.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { CidadeService } from 'src/app/services/domain/cidade.service';
+import { EstadoService } from 'src/app/services/domain/estado.service';
+import { EstadoDTO } from 'src/app/models/estado.dto';
+import { CidadeDTO } from 'src/app/models/cidade.dto';
 
 @Component({
   selector: 'app-signup',
@@ -9,8 +13,13 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 export class SignupPage implements OnInit {
 
   formGroup: FormGroup;
+  estados: EstadoDTO[];
+  cidades: CidadeDTO[];
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(
+    private formBuilder: FormBuilder,
+    private cidadeService: CidadeService,
+    private estadoService: EstadoService) {
     this.formGroup = this.formBuilder.group({
       nome: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(120)]],
       email: ['', [Validators.required, Validators.email]],
@@ -25,12 +34,29 @@ export class SignupPage implements OnInit {
       telefone1: ['', [Validators.required]],
       telefone2: ['', []],
       telefone3: ['', []],
-      estadoId: [null, [Validators.required]],
-      cidadeId: [null, [Validators.required]]
+      estadoId: ['', [Validators.required]],
+      cidadeId: ['', [Validators.required]]
     });
    }
 
   ngOnInit() {
+    this.estadoService.findAll()
+      .subscribe(response => {
+        this.estados = response;
+        this.formGroup.controls.estadoId.setValue(this.estados[0].id);
+        this.updateCidades();
+      },
+      error => {});
+  }
+
+  updateCidades() {
+    let estado_id = this.formGroup.value.estadoId;
+    this.cidadeService.findAll(estado_id)
+      .subscribe(response => {
+        this.cidades = response;
+        this.formGroup.controls.cidadeId.setValue(null);
+      },
+      error => {});
   }
 
   signupUser() {
