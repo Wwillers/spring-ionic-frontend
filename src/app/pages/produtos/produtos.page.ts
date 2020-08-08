72f7ff4acb803ecfca94394f13466d15323736af
William <w.willers@outlook.com>
2020-08-08 11:56:20 -0400

    Carregando imagens dos produtos

--- src/app/pages/produtos/produtos.page.ts
+++ src/app/pages/produtos/produtos.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ProdutoDTO } from 'src/app/models/produto.dto';
 import { ProdutoService } from 'src/app/services/domain/produto.service';
 import { ActivatedRoute } from '@angular/router';
+import { API_CONFIG } from 'src/app/config/api.config';
 
 @Component({
   selector: 'app-produtos',
@@ -19,8 +20,20 @@ export class ProdutosPage implements OnInit {
     this.produtoService.findByCategoria(categoria_id)
       .subscribe(response => {
         this.items = response['content'];
+        this.loadImageUrls();
       },
       error => {});
+  }
+
+  loadImageUrls () {
+    for (var i=0; i<this.items.length; i++) {
+      let item = this.items[i];
+      this.produtoService.getSmallImageFromBucket(item.id)
+        .subscribe(response => {
+          item.imageUrl = `${API_CONFIG.bucketBaseUrl}/prod${item.id}-small.jpg`
+        },
+        error => {});
+    }
   }
 
 }
